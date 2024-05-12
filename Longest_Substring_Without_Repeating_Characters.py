31a64eb737c9f3ace7a0fa5d998bbc23ca24ad40
abhiram070 <145694840+abhiram070@users.noreply.github.com>
2024-05-12 08:15:14 +0530

    practice

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -1,1 +1,17 @@
-print("hello world")
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        d = ""
+        for i in range (1,len(s)):
+            count = 0
+            for j in range (len(d)):
+                if s[i] == d[j]:
+                    count = 1
+                    break
+            if count == 0:
+                d = d + s[i]           
+        return len(d)
+
+sol = Solution()
+s = "abcabcbb"
+result = sol.lengthOfLongestSubstring(s)
+print("The answer is ",result)      
